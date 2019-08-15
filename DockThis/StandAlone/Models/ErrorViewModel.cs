265aa4bfb15e151aa991691be98d5dbbefe15bae
Lucas Sadle <lucas.sadle@capstonec.com>
2019-08-15 15:45:16 -0500

    Init Solution and StandAlone web app

---
+++ DockThis/StandAlone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StandAlone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
